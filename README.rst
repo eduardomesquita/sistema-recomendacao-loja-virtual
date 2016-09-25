74996e5139c38171ee081233af6e019d24ce78bc
Eduardo Mesquita <eduardomesquita09@gmail.com>
2016-09-25 11:54:27 -0300

    Adicionando padrão pep-8

---
+++ README.rst
@@ -0,0 +1,31 @@
+.. -*- mode: rst -*-
+
+Sistema de Recomendação Para uma loja virtual
+============
+
+sistema-recomendacao-loja-virtual or (SRLV) is a Python module for machine learning built on top of
+scikit-learn and Pandas.
+
+Installation
+------------
+
+Dependencies
+~~~~~~~~~~~~
+
+SRLV requires::
+
+- Python (>= 3.3),
+- Pandas (>= 0.18.1),
+- scikit-learn (>= 0.17.1).
+
+
+User installation
+~~~~~~~~~~~~~~~~~
+
+If you already have a working installation of ``pip`` ::
+
+    pip install -U numpy scipy scikit-learn
+
+and ::
+
+    pip install pandas
--- recomendacao.py
+++ recomendacao.py
@@ -1,61 +1,51 @@
-#pip3 install pandas
-#pip3 install -U numpy scipy scikit-learn
-import sys
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import pandas as pd
 import argparse
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
-
-
 class Train(object):
-	
-	def __init__(self, path):
-		try:
-			self._ds = pd.read_csv(path)
-			self._results = {}
-		except:
-			exit()
 
-	def train(self):
-		tf = TfidfVectorizer(analyzer='word', 
-				ngram_range=(1, 3), 
-				min_df=0, 
-				stop_words='english')
+    def __init__(self, path):
+        self._ds = pd.read_csv(path)
+        self._results = {}
 
-		tfidf_matrix = tf.fit_transform(self._ds['description'])
-		cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
+        self._train()
 
-		for idx, row in self._ds.iterrows():
-		    similar_indices = cosine_similarities[idx].argsort()[:-100:-1]
-		    similar_items = [(cosine_similarities[idx][i], self._ds['id'][i]) for i in similar_indices]
+    def _train(self):
+        tf = TfidfVectorizer(analyzer='word',
+                             ngram_range=(1, 3),
+                             min_df=0,
+                             stop_words='english')
 
-		    # First item is the item itself, so remove it.
-		    # Each dictionary entry is like: [(1,2), (3,4)], with each tuple being (score, item_id)
-		    self._results[row['id']] = similar_items[1:]
+        tfidf_matrix = tf.fit_transform(self._ds['description'])
+        cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-	def getItem(self, _id):
-    		return self._ds.loc[self._ds['id'] == _id]['description'].tolist()[0].split(' - ')[0]
+        for idx, row in self._ds.iterrows():
+            similar_indices = cosine_similarities[idx].argsort()[:-100:-1]
+            similar_items = [(cosine_similarities[idx][i], self._ds['id'][i]) for i in similar_indices]
 
+            self._results[row['id']] = similar_items[1:]
 
-	def recommend(self, item_id, num):
-		print('Recomendando ' + str(num) + ' produtos similares com: ' + self.getItem(item_id) + '...')
-		print('-' * 5)
-		recs = self._results[item_id][:num]
-		for rec in recs:
-			print('Recomendado: ' + self.getItem(rec[1]) + ' (pontuação:' + str(rec[0]) + ')')
-	
+    def _getItem(self, _id):
+        return self._ds.loc[self._ds['id'] == _id]['description'].tolist()[0].split(' - ')[0]
+
+    def recommend(self, item_id, num):
+        print('Recomendando ' + str(num) + ' produtos similares com: ' + self._getItem(item_id) + '...')
+        print('-' * 5)
+        recs = self._results[item_id][:num]
+        for rec in recs:
+            print('Recomendado: ' + self._getItem(rec[1]) + ' (pontuação:' + str(rec[0]) + ')')
 
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Process some integers.')
-	parser.add_argument('--item', type=int, help='Id do item')
-	parser.add_argument('--qtd', type=int, help='Quantidade de recomendações')
-	parser.add_argument('--path', type=str, help='Caminho do arquivo dataSet')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--idItem', type=int, help='Id do item')
+    parser.add_argument('--nResult', type=int, help='Quantidade de recomendações')
+    parser.add_argument('--path', type=str, help='Caminho do arquivo dataSet')
+    args = parser.parse_args()
 
-	t = Train(args.path)
-
-	t.train()
-	t.recommend(args.item, args.qtd)
-
+    t = Train(args.path)
+    t.recommend(args.item, args.qtd)
